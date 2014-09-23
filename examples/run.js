1273cae40621e6ffc329d4bed9a9bf8e4eea8701
Mohsen Azimi <mazimi@apigee.com>
2014-09-23 15:14:00 -0700

    House keeping

--- examples/run.js
+++ examples/run.js
@@ -1,15 +1,22 @@
 
-var path = process.argv[2],
-    fs = require('fs'),
-    yaml = require('../lib/yaml')
-    
-if (!path)
-  throw new Error('provide path to yaml file')
+var fs = require('fs');
+var path = require('path');
+var  yaml = require('../lib/yaml');
 
-fs.readFile(path, function(err, fileContents) {
-  fileContents = fileContents.toString()
-  console.log('\n')
-  console.log(fileContents)
-  console.log('\noutputs:\n')
-  console.log(yaml.eval(fileContents))
-})
+fs.readdir(__dirname, function (err, files) {
+  files.forEach(function (file) {
+    if(file.indexOf('yaml')){
+      fs.readFile(path.join(__dirname, file), function(err, fileContents) {
+        fileContents = fileContents.toString()
+        console.log('\n')
+        console.log(fileContents)
+        console.log('\nparse outputs:\n')
+        console.log(yaml.eval(fileContents))
+        console.log('\n')
+        console.log('\nast outputs:\n')
+        console.log(yaml.ast(fileContents))
+      });
+    }
+  });
+});
+
--- try.js
+++
@@ -1,8 +0,0 @@
-var jsyaml = require('./lib/yaml.js');
-
-
-var yaml = 'me: 1';
-
-var result = jsyaml.ast(yaml);
-console.log(result.constructor.name)
-console.log(result);
