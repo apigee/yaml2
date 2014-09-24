24e056a6f2f78f3b9b1882038d8edc65a3359be4
Mohsen Azimi <mazimi@apigee.com>
2014-09-24 08:20:15 -0700

    WIP

--- examples/run.js
+++ examples/run.js
@@ -2,19 +2,21 @@
 var fs = require('fs');
 var path = require('path');
 var  yaml = require('../lib/yaml');
+var util = require('util');
+
 
 fs.readdir(__dirname, function (err, files) {
   files.forEach(function (file) {
-    if(file.indexOf('yaml')){
+    if(file.indexOf('yaml') > -1){
       fs.readFile(path.join(__dirname, file), function(err, fileContents) {
         fileContents = fileContents.toString()
         console.log('\n')
         console.log(fileContents)
         console.log('\nparse outputs:\n')
-        console.log(yaml.eval(fileContents))
+        console.log(util.inspect(yaml.eval(fileContents), {showHidden: false, depth: null}));
         console.log('\n')
         console.log('\nast outputs:\n')
-        console.log(yaml.ast(fileContents))
+        console.log(util.inspect(yaml.ast(fileContents), {showHidden: false, depth: null}));
       });
     }
   });
