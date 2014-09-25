b8e04473329ab6dda5191f9dd2f5d54116c6ad29
Mohsen Azimi <mazimi@apigee.com>
2014-09-25 13:06:20 -0700

    Update tests

--- examples/run.js
+++ examples/run.js
@@ -4,23 +4,29 @@ var path = require('path');
 var  yaml = require('../lib/yaml');
 var util = require('util');
 
+if (process.argv[2]) {
+  return run(process.argv[2]);
+}
 
 fs.readdir(__dirname, function (err, files) {
   files.forEach(function (file) {
     if(file.indexOf('yaml') > -1){
-      fs.readFile(path.join(__dirname, file), function(err, fileContents) {
-        fileContents = fileContents.toString()
-        console.log('\n')
-        console.log(fileContents)
-        console.log('\nparse outputs:\n')
-        var parsed = yaml.eval(fileContents);
-        console.log(util.inspect(parsed, {showHidden: false, depth: null}));
-        console.log('\n')
-        console.log('\nast outputs:\n')
-        var asted = yaml.ast(fileContents);
-        console.log(util.inspect(asted, {showHidden: false, depth: null}));
-      });
+      run(path.join(__dirname, file))
     }
   });
 });
 
+function run(filePath){
+  fs.readFile(filePath, function(err, fileContents) {
+    fileContents = fileContents.toString()
+    console.log('\n')
+    console.log(fileContents)
+    console.log('\nparse outputs:\n')
+    var parsed = yaml.eval(fileContents);
+    console.log(util.inspect(parsed, {showHidden: false, depth: null}));
+    console.log('\n')
+    console.log('\nast outputs:\n')
+    var asted = yaml.ast(fileContents);
+    console.log(util.inspect(asted, {showHidden: false, depth: null}));
+  });
+}
